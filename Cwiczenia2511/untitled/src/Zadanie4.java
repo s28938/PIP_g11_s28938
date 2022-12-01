f87fa3c1917461e1a53adb2ebf4c9758786a59d5
s28938 <118555515+s28938@users.noreply.github.com>
2022-12-01 19:06:24 +0100

    Update Zadanie4.java
    poprawki

--- Cwiczenia2511/untitled/src/Zadanie4.java
+++ Cwiczenia2511/untitled/src/Zadanie4.java
@@ -1,24 +1,66 @@
+import java.util.Scanner;
+
 public class Zadanie4 {
-    public static void main(String[] args) {
-        int x, y, z, i, j;
-        x = 5;
-        y = 3;
-        z = 8;
-        for (i = 0; i < z; i++) {
-            for (j = 0; j < y; j++) {
-                if (x < z) {
-                    System.out.println("  ");
-                } else {
-                    System.out.println("* ");
-                }
-                if (y < z) {
-                    System.out.println("  ");
-                } else {
-                    System.out.println("* ");
-                }
-                System.out.println("* ");
-            }
-            System.out.println("\n");
-        }
+
+    public static void main( String [] args ) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] values = new int[3];
+        System.out.println("Wprowadz wartosc x:");
+        int currentItem1 = scanner.nextInt();
+        System.out.println("Wprowadz wartosc y:");
+        int currentItem2 = scanner.nextInt();
+        System.out.println("Wprowadz wartosc z:");
+        int currentItem3 = scanner.nextInt();
+        values[0] = currentItem1;
+        values[1] = currentItem2;
+        values[2] = currentItem3;
+
+        drawStars(values);
+
     }
+
+    public static void drawStars( int[] values ) {
+
+        int rows = values.length;
+        int maxValue = findMax( values );
+
+        String[] horizontalRow = new String[ maxValue + 1];
+
+        for( int row = 0; row <= maxValue; row++ ) {
+
+            String patternRow = "";
+            int barRow = maxValue - row;
+            for( int column = 0; column < values.length; column++ ) {
+
+                if( barRow < values[ column ] ) {
+                    patternRow += "* ";
+                } else {
+                    patternRow += "  ";
+                }
+            }
+
+            horizontalRow[ row ] = patternRow;
+
+        }
+
+        for( String pattern : horizontalRow ) {
+            System.out.println( pattern );
+        }
+
+    }
+
+    public static int findMax( int[] values ) {
+
+        int max = 0;
+        for( int value : values ) {
+            if( max < value ) {
+                max = value;
+            }
+        }
+
+        return max;
+    }
+
 }
+
