f7ba1dbf83080e7d828a7eeabe68546bf7e6fcb8
s28938 <118555515+s28938@users.noreply.github.com>
2022-12-08 19:40:11 +0100

    Update Zadanie4.java
    Poprawione

--- Cwiczenia0212/src/Zadanie4.java
+++ Cwiczenia0212/src/Zadanie4.java
@@ -1,30 +1,29 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Zadanie4 {
     public static void main(String[] args) {
         int[] genArr = generateArray();
+        System.out.println("Wygenerowana tablica");
         printArray(genArr);
         int[] sortedArr = sort(genArr);
+        System.out.println("Posortowana tablica");
         printArray(sortedArr);
         int[] revArr = reverse(sortedArr);
+        System.out.println("Odwrocona tablica");
         printArray(revArr);
-        System.out.println(checkReversed(sortedArr,revArr));
+        System.out.println("Czy tablice sa swoimi odwrotnosciami");
+        System.out.println(equalsReverse(sortedArr,revArr));
     }
-    public static boolean checkReversed(int[] x, int[] y)
+    public static boolean equalsReverse(int[] x, int[] y)
     {
-        // For production code, possibly add nullity checks here (see comments)
         if (x.length != y.length)
         {
             return false;
         }
-        // Loop through x forwards and y backwards
         for (int i = 0; i < x.length; i++)
         {
             if (x[i] != y[y.length - 1 - i])
             {
-                // As soon as we've found a "mistake" we can exit:
-                // This is simpler (IMO) than keeping a "check" variable
                 return false;
             }
         }
@@ -32,33 +31,32 @@ public class Zadanie4 {
     }
     public static int [] reverse (int[] array){
 
-        //System.out.println("Twoja tablica to: ");
-        for (int i=0;i<array.length;i++){
-            System.out.print(array[i]+ " ");
-        }
-        System.out.println();
-        //System.out.println("Tablica w odwrotnej kolejnosci: ");
-        for (int i = array.length-1; i >= 0; i--){
-            System.out.print(array[i] + " ");
-        }
-        return array;
+        int [] kopia = Arrays.copyOf(array,array.length);
+        for(int i = 0; i < kopia.length / 2; i++)
+        {
+            int temp = kopia[i];
+            kopia[i] = kopia[kopia.length - i - 1];
+            kopia[kopia.length - i - 1] = temp;
+           }
+        return kopia;
     }
     public static int [] sort (int[] array) {
-        for (int i = 0; i <array.length; i++){
+        int [] kopia = Arrays.copyOf(array,array.length);
+        for (int i = 0; i <kopia.length; i++){
             int index = i;
-            for (int j = i ; j <= array.length-1; j++){
-                if (array[j] < array[index]){
+            for (int j = i ; j <= kopia.length-1; j++){
+                if (kopia[j] < kopia[index]){
                     index = j;
                 }
             }
-            int temp = array[i];
-            array[i] = array[index];
-            array[index] = temp;
+            int temp = kopia[i];
+            kopia[i] = kopia[index];
+            kopia[index] = temp;
         }
-        for (int i = 0; i < array.length; i++){
-            System.out.print(array[i] + " ");
-        }
-        return array;
+        //for (int i = 0; i < kopia.length; i++){
+        //    System.out.println(kopia[i]);
+        //}
+        return kopia;
     }
     public static int [] generateArray(){
         Scanner skanowanko = new Scanner(System.in);
