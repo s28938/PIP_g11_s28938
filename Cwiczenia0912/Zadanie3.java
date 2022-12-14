596f2166ff10e3eef761c1ea7b3d0076217be32b
s28938 <118555515+s28938@users.noreply.github.com>
2022-12-14 18:31:21 +0100

    Update Zadanie3.java
    poprawione

--- Cwiczenia0912/Zadanie3.java
+++ Cwiczenia0912/Zadanie3.java
@@ -1,36 +1,25 @@
 import java.util.Arrays;
 public class Zadanie3 {
-    public static void ReverseArray(int arr[])
-    {
-        int i;
-        int n = arr.length;
-        if(n>0)
-        {
-            i=n-1;
-            System.out.print(arr[i]+" ");
-            i--;
-            ReverseArray(arr);
-        }
-    }
-    public static void main(String[] args) {
-        int arr[]= {0,1,2,3,4};
-        System.out.print("Provided array: "+ Arrays.toString(arr));
-        System.out.println();
-        System.out.print("After reversing Array Element Are:");
-        ReverseArray(arr);
-    }
-    public static void ReverseArray2(int arr2[])
-    {
-        int i;
-        int n = arr2.length;
-        if(n>0)
-        {
-            i=n-1;
-            System.out.print(arr2[i]+" ");
-            i--;
-            ReverseArray(arr2);
-        }
-    }
-}
 
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 2, 3, 4};
+        System.out.print("Provided array: " + Arrays.toString(arr));
+        System.out.println();
+        reverseArray(arr);
+        System.out.print("Reversed array: " + Arrays.toString(arr));
+    }
+    public static void reverseArray(int[] x){
+        reverse(x, 0, x.length -1);
+    }
+
+    public static void reverse(int[] x, int i, int j){
+        if(i<j){//Swap
+            int tmp = x[i];
+            x[i] = x[j];
+            x[j] = tmp;
+            reverse(x, ++i, --j);//Recursive
+        }
+    }
+
+}
 
